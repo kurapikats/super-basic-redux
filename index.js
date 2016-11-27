2cbdab60e11a60c66dff417f557dfd33296e84c6
kurapikats <kurapikats@yahoo.com>
2016-11-27 12:44:59 +0800

    add middleware

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 let createStore = require('redux').createStore;
+let applyMiddleware = require('redux').applyMiddleware;
 
-const reducer = (state, action) => {
-  if (action.type === "INC") {
-    return state + action.payload;
-  }
-  if (action.type === "DEC") {
-    return state - action.payload;
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INC":
+      return state + action.payload;
+      break;
+    case "DEC":
+      return state - action.payload;
+      break;
+    case "ERR":
+      throw new Error("This is an error message.");
+      break;
   }
   return state;
 }
 
-const store = createStore(reducer, 0);
+const logger = (store) => (next) => (action) => {
+  console.log("action fired", action);
+  next(action);
+}
+
+const error = (store) => (next) => (action) => {
+  try {
+    next(action);
+  } catch (e) {
+    console.log("Error", e);
+  }
+}
+
+let middleware = applyMiddleware(logger, error);
+
+const store = createStore(reducer, 0, middleware);
 
 store.subscribe(() => {
   console.log("store changed", store.getState());
@@ -22,4 +43,5 @@ store.dispatch({type: "INC", payload: 3});
 store.dispatch({type: "INC", payload: 56});
 store.dispatch({type: "DEC", payload: 2});
 store.dispatch({type: "DEC", payload: 22});
-store.dispatch({type: "DEC", payload: 1});
+store.dispatch({type: "ERR"});
+store.dispatch({type: "INC", payload: 22});
